855f58604cf482968d871a1cc178cebce96438c8
Yunyan <708931190@qq.com>
2024-09-18 11:28:00 +0800

    axios项目初始化

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MyAxios from './components/my-axios.vue'
+</script>
+
+<template>
+  <MyAxios></MyAxios>
+</template>
+
+<style scoped></style>
---
+++ src/components/my-axios.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Axios 测试</div>
+</template>
+
+<style scoped>
+
+</style>
